f1ba1618044c63bb2c92af2e1b8cdde1416f6cfe
SSCharan-99 <saisricharan99@msitprogram.net>
2021-08-09 15:26:56 +0530

    ismultiple commit

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -4,6 +4,16 @@
 # Also, you should make constructive use of the isFactor function you just wrote above.
 
 
-
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if m==0:
+		return True
+	elif m>=1 and n==0:
+		return False
+	elif m%n==0:
+		return True
+	else:
+		return False # replace with your solution
+
+
+	
+
