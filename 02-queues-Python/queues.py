ca5eb0b62f8a04c48753e7148b17e279e89e5ead
SSCharan-99 <72122904+SSCharan-99@users.noreply.github.com>
2021-08-15 22:23:49 +0530

    Update queues.py

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -3,16 +3,15 @@ Hint: You can use any Python list method
 you'd like! Try to write each one in as 
 few lines as possible.
 Make sure you pass the test cases too!"""
-
 class Queue:
-    def __init__(self, head=None):
+    def _init_(self, head=None):
         self.storage = [head]
 
     def enqueue(self, new_element):
-        pass
+        self.storage.insert(0,new_element)
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        return self.storage.pop()
