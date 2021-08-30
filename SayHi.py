782b4b89cbc3e26af61951cfde1e72dc1b4d63cd
SSCharan-99 <saisricharan99@msitprogram.net>
2021-08-30 17:07:12 +0530

    hi told

--- SayHi.py
+++ SayHi.py
@@ -1,8 +1,22 @@
-# Write a class called Dog that has two properties: name and age. Write a constructor that takes three arguments self, name and age and set these to the object properties.
-# Now write a function sayHI(dog) where dog is the dog class object that returns a Hi, my name is <dog’s name> and I am <age> years old!
+# Write a class called Dog that has two properties: name and age.
+# Write a constructor that takes three arguments self, name and age and set these to the object properties.
+# Now write a function sayHI(dog) where dog is the dog class object that
+# returns a Hi, my name is <dog’s name> and I am <age> years old!
 
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-class SayHi(Object):
-    pass
+class SayHi:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+
+    def sayHi(self):
+        print("Hi, my name is",self.name,"and I am",self.age,"years old")
+
+
+ob=SayHi("Dog","4")
+ob.sayHi()
+ob1=SayHi("Elf","3")
+ob1.sayHi()
